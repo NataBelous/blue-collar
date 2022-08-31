eba06ca3a19bd4dd50996ddefcef9950520ed6ac
NataBelous <104021063+NataBelous@users.noreply.github.com>
2022-08-31 15:41:11 +0000

    Deploying to gh-pages from @ NataBelous/blue-collar@22a99a6d184d4e5e5f812adf0767d8a4ad124475 🚀

---
+++ animation.2dc48b71.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("7Urzm",(function(e,i){AOS.init()}));
+//# sourceMappingURL=animation.2dc48b71.js.map
---
+++ animation.3e904273.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("jnUW4",(function(e,n){AOS.init()}));
+//# sourceMappingURL=animation.3e904273.js.map
